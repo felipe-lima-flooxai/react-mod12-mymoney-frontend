199fe1206ee83880a92c4bc9eb5fb194930f3978
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-30 14:28:14 -0300

    feat: footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2017
+            <a href="semsite.com" target="_blank">SemSite</a>
+        </strong>
+    </footer>
+)
