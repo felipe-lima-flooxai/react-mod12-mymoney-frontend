b744c06f53fe57f696f13e6e58815f27ba18345d
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-02 11:00:28 -0300

    feat: varios componentes tabs

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -4,6 +4,9 @@ import React, {Component} from "react";
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
 class BillingCycle extends Component {
     render(){
         return (
