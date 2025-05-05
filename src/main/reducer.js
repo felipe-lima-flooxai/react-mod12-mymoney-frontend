e2539a8208de646ff4ba110b3d3710887e937ee1
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-05 12:44:07 -0300

    feat: mensagens de erro

--- src/main/reducer.js
+++ src/main/reducer.js
@@ -3,12 +3,14 @@ import {reducer as formReducer} from "redux-form"
 import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from '../common/tab/tabReducer'
 import billingCycleReducer from "../billingCycle/billingCycleReducer";
+import {toastr, reducer as toastrReducer} from "react-redux-toastr"
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: billingCycleReducer,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 })
 
 export default rootReducer
