c8701e2e1479381a632a2163f34cb845791f50c9
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-05 14:32:10 -0300

    feat: criar, upar e excluir no forms

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -13,20 +13,32 @@ export function getList() {
     }
 }
 
-export function create(values){
+
+
+function submit(values, method) {
     return dispatch => {
-        axios.post(`${BASE_URL}/billingCycles`, values).then(resp=>{
-            toastr.success("Sucesso", "Opreação Realizada com Sucesso")
-            dispatch([
-                resetForm("billingCycleForm"), 
-                getList(),
-                selectTab("tabList"),
-                showTabs("tabList", "tabCreate")
-            ])
-        }).catch(e=>{
-            e.response.data.errors.forEach(error => toastr.error("Erro", error))
-        })
+        const id = values._id ? values._id : ''
+        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação Realizada com sucesso.')
+                dispatch(init())
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
     }
+}
+
+export function create(values) {
+    return submit(values, 'post')
+}
+
+export function update(values) {
+    return submit(values, 'put')
+}
+
+export function remove(values) {
+    return submit(values, 'delete')
 }
 
 export function showUpdate(billingCycle) {
