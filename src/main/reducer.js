6af0568845d803760de729b8c4c89ac069f39f1c
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-06 11:18:40 -0300

    feat: actions de autenticação

--- src/main/reducer.js
+++ src/main/reducer.js
@@ -4,13 +4,16 @@ import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from '../common/tab/tabReducer'
 import billingCycleReducer from "../billingCycle/billingCycleReducer";
 import {toastr, reducer as toastrReducer} from "react-redux-toastr"
+import AuthReducer from "../auth/authReducer";
+import authReducer from "../auth/authReducer";
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: billingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    auth: authReducer
 })
 
 export default rootReducer
